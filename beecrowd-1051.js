9b6d04a3702c1e68dcd983a13d727986c921c1d6
taibislamdipu <mailtaibislam@gmail.com>
2023-06-02 10:47:12 +0600

    Solution: Beecrowd-1051. Tax calculation.

---
+++ beecrowd-1051.js
@@ -0,0 +1,40 @@
+// In an imaginary country called Lisarb, all the people are very happy to pay their taxes because they know that doesn’t exist corrupt politicians and the taxes are used to benefit the population, without any misappropriation. The currency of this country is Rombus, whose symbol is R$.
+
+// Read a value with 2 digits after the decimal point, equivalent to the salary of a Lisarb inhabitant. Then print the due value that this person must pay of taxes, according to the table below.
+
+// Remember, if the salary is R$ 3,002.00 for example, the rate of 8% is only over R$ 1,000.00, because the salary from R$ 0.00 to R$ 2,000.00 is tax free. In the follow example, the total rate is 8% over R$ 1000.00 + 18% over R$ 2.00, resulting in R$ 80.36 at all. The answer must be printed with 2 digits after the decimal point.
+
+// Input
+// The input contains only a float-point number, with 2 digits after the decimal point.
+
+// Output
+// Print the message "R$" followed by a blank space and the total tax to be payed, with two digits after the decimal point. If the value is up to 2000, print the message "Isento".
+
+// Input Samples
+// 3002.00
+
+// Outputs Samples
+// R$ 80.36
+
+function getTax(salary) {
+  let taxPercent;
+  let taxAmount;
+
+  if (salary <= 2000) {
+    return "Isento";
+  } else if (salary <= 3000) {
+    taxPercent = 0.08;
+    taxAmount = (salary - 2000) * taxPercent;
+    return `R$ ${taxAmount.toFixed(2)}`;
+  } else if (salary <= 4500) {
+    taxPercent = 0.18;
+    taxAmount = 1000 * 0.08 + (salary - 3000) * taxPercent;
+    return `R$ ${taxAmount.toFixed(2)}`;
+  } else {
+    taxPercent = 0.28;
+    taxAmount = 1000 * 0.08 + 1500 * 0.18 + (salary - 4500) * taxPercent;
+    return `R$ ${taxAmount.toFixed(2)}`;
+  }
+}
+
+console.log(getTax(4520));
