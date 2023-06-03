6494d69342656881e19f35dc3401aeb2e50a79ec
taibislamdipu <mailtaibislam@gmail.com>
2023-06-03 11:17:35 +0600

    Solution: Beecrowd-1052. Get array position, first letter in uppercase.

--- beecrowd-1052.js
+++ beecrowd-1052.js
@@ -1,7 +1,7 @@
 // Read an integer number between 1 and 12, including. Corresponding to this number, you must print the month of the year, in english, with the first letter in uppercase.
 
 // Input
-// The input contains only an integer number..
+// The input contains only an integer number.
 
 // Output
 // Print the name of the month according to the input number, with the first letter in uppercase.
